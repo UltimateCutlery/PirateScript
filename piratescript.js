2036f31bf0dfa5bbbd9a89411cbc29fea4fe70c7
UltimateCutlery <57415618+UltimateCutlery@users.noreply.github.com>
2019-12-03 16:25:10 +0000

    pirate_attempt1

--- piratescript.js
+++ piratescript.js
@@ -1,8 +1,25 @@
 // MAKE SURE THIS REMAINS UNCHANGED
-
-
+ 
 function piratize(input){
 
-
+    var words = input.split(" "); // split input into an array of individual words
     
-} 
+    var piratewords = words.map(function(word){
+            if (isNaN(word)==false){
+                return word
+            } else if (word == "pounds"){
+                word = "dubloons";
+                return word; // if the word is pounds, replaces with dubloons
+            } else { 
+                word = word.substr(1) + word.substr(0,1) + "arr";
+                return word; //moves first letter to end and adds arr
+            }
+    });
+    var piratephrase = piratewords.join(" ");
+    
+    return piratephrase;
+    };
+
+
+
+
